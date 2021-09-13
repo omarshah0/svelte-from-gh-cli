edc05c119d589f3e134bc3e5a87c347a5f8c8814
Omar Shah <60596111+omarshah0@users.noreply.github.com>
2021-09-13 11:33:05 +0500

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from './Components/Header';
+</script>
+
+<main>
+  <Header />
+</main>
+
+<style>
+</style>
---
+++ src/Components/Header/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<header>
+  <div class="inner">
+    <h1>This is Svelte By Omar</h1>
+  </div>
+</header>
+
+<style>
+  header {
+    background-color: aqua;
+    padding: 2rem 3rem;
+  }
+  h1 {
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+  .inner {
+    max-width: 1536px;
+    margin: 0 auto;
+  }
+</style>
